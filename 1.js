96d2034a086f68d3e5609c7f1ed6efdad1470376
krish556 <68814130+krish556@users.noreply.github.com>
2020-07-26 20:18:10 +0545

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="1.css" />
+</head>
+<body> 
+<div>
+    <div style="height:40;background-color: red"><center><h3 style="color:white;padding-top: 8px">Login</h3></center></div>
+    <br>
+    <div style="margin-left:20px;margin-right: 20px">
+    
+        <form method="post" action="#" autocomplete="off">
+    
+                      <input class="int" type="text" name="email" placeholder="first name" required>
+                       <input class="int" type="text" name="email" placeholder="last name" required>
+	    <input class="int" type="email" name="email" placeholder="tony@gmail.com" required>
+                    <input class="int" type="tel"  name="phone" pattern="[7|8|9][0-9]{9}">
+ 
+                     <input class="int" type="password" name="password" placeholder="Password" required>
+                             
+                    <input class="int" style="cursor: pointer;background-color:blue;border: 2px;border-radius: 5px;color: white" type="submit" value="Login">
+            
+         </form>
+    </div>
+    <h4 style="color:gainsboro">OR</h4> 
+    </div>
+  
+ 
+                    <div  style="background-color:#3B5998;border: 2px;border-radius: 5px;margin-left: 20px;margin-right: 20px" > 
+                        <a href="#" class="long" ><center>
+                            <i  style="color: white;" class="fa fa-facebook fa-fw"></i></center>
+                        </a>    
+
+                    </div>
+      <br>
+                    <div style="background-color:#dd4b39;border: 2px;border-radius: 5px;margin-left:20px;margin-right: 20px">
+                        <a href="#" class="long"  ><center>
+                          <i style="color: white;" class="fa fa-google fa-fw"></i></center>
+                        </a>
+                    </div>
+      <br>
+      <br>
+                    <div  style="margin-left:20px;margin-right: 20px">   
+                        <button class="long"
+                        style="cursor: pointer;background-color:#00ff00;border: 2px;border-radius: 5px;">&nbsp;&nbsp;&nbsp;&nbsp;< EPAM LOGIN ></button>
+                    </div>
+    </div>
+    </body>
+</html>
+
+<!-- Styles -->
